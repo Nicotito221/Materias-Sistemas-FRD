240ba6befacd1d1985549ec06830643657ac60d8
Nicolas Angerossa <nangerossa@frd.utn.edu.ar>
2026-01-07 17:44:54 -0300

    Ajustes para deploy en vercel

--- prisma/migrations/20260107201249_add_aplazo_details/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "MateriaProgreso" ADD COLUMN "notaAplazo1" REAL;
-ALTER TABLE "MateriaProgreso" ADD COLUMN "notaAplazo2" REAL;
-ALTER TABLE "MateriaProgreso" ADD COLUMN "notaAplazo3" REAL;
